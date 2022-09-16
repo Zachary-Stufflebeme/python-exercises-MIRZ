164474eb39e7e93f85954353f8667f994f96a03b
Zachary Stufflebeme <Stufflebeme.Zachary@gmail.com>
2022-09-16 11:01:34 -0500

    Here I practice using python

---
+++ data_types_and_variables.py
@@ -0,0 +1,33 @@
+           #1
+return((3*3) + (3*5) + (3*1))
+           #2
+google = 400
+amazon = 380
+facebook = 350
+
+(google * 6) + (amazon * 4) + (facebook * 10)
+           #4
+bool(class_is_full)
+bool(schedule_conflict)
+bool(can_enroll)
+
+if class_is_full and schedule_conflict == False
+   can_enroll = True
+          #5   
+bool(premium)
+bool(offer_valid)
+bool(more_than_two)
+bool(apply)
+
+if offer_valid and premium == True
+    apply = True
+if offer_valid and more_than_two == True
+    apply = True
+         #6
+bool(five_char)
+bool(most_twenty)
+bool(user_and_pass)
+bool(accept_login)
+
+if(five_char) and (most_twenty) and (user_and_pass) == True 
+    accept_login = True
